7077cb3d4a50a993cfdb5f3a3074e08ad0d9fc2f
ricejake <ecirbocaj@yahoo.com>
2021-07-11 12:55:50 -0500

    Part 1: Connect a Database to a Spring App: Complete (time zone issue resolved)

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Max;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @Max(1)
+    String location;
+
+
+    public Employer() {}
+
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 }
